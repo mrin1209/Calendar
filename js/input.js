9f4b4f90ee26eea0ddf882c5bfec9a93544a11cd
mrin1209 <104071894+mrin1209@users.noreply.github.com>
2022-12-08 15:31:01 +0900

    Add files via upload

--- js/input.js
+++ js/input.js
@@ -1,7 +1,7 @@
 class Input {
   constructor() {
     const inputTab = document.querySelectorAll('.inputTab');
-
+    const inputList = document.querySelectorAll('.inputList');
     document.addEventListener('click',function(e) {
       if (e.target.className === 'addMoney') {
         if (screenMode === 'calendar') {
@@ -16,6 +16,12 @@ class Input {
       element.addEventListener('click',function(e) {
         document.querySelector('.inputSelect').classList.remove('inputSelect');
         e.target.classList.add('inputSelect');
+      })
+    })
+
+    inputList.forEach(function(element) {
+      element.addEventListener('click',function(e) {
+        console.log('a');
       })
     })
   }
