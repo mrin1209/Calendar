eb3525a6c6bc4bb4ceb4e91db387880f1692adc8
mrin1209 <104071894+mrin1209@users.noreply.github.com>
2022-12-08 15:07:15 +0900

    Add files via upload

--- js/input.js
+++ js/input.js
@@ -1,5 +1,7 @@
 class Input {
   constructor() {
+    const inputTab = document.querySelectorAll('.inputTab');
+
     document.addEventListener('click',function(e) {
       if (e.target.className === 'addMoney') {
         if (screenMode === 'calendar') {
@@ -9,9 +11,17 @@ class Input {
         }
       }
     }) 
+
+    inputTab.forEach(function(element) {
+      element.addEventListener('click',function(e) {
+        document.querySelector('.inputSelect').classList.remove('inputSelect');
+        e.target.classList.add('inputSelect');
+      })
+    })
   }
 
   enterAmount(year,month,date) {
     console.log(year,month,date);
+    
   }
 }
