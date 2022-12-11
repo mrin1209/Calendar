b51bd1fd17e2f3c01f1c863235dd7d698b27ee8a
mrin1209 <104071894+mrin1209@users.noreply.github.com>
2022-12-12 07:54:32 +0900

    Add files via upload

--- js/input.js
+++ js/input.js
@@ -6,6 +6,7 @@ class Input {
     this.inputTabsub = document.querySelector('.inputTabsub');
     const inputBtn = document.querySelector('.inputBtn');
     this.loopMoney = document.querySelector('.loopMoney');
+    this.loopMoneyLimit = document.querySelector('.loopMoneyLimit');
     this.closeBtn = document.querySelector('.closeBtn');
     this.inputError = document.querySelector('.errorMessage');
     this.loopMoney = document.querySelector('.loopMoney');
@@ -20,9 +21,11 @@ class Input {
         input.editMode = false;
         input.closeBtn.textContent = '閉じる';
         if (screenMode === 'calendar') {
-          input.inputMoneyValue('',`${year}-${( '00' + (month) ).slice( -2 )}-${( '00' + calendar.clickDate ).slice( -2 )}`,'',1);
+          let inputDate = `${year}-${( '00' + (month) ).slice( -2 )}-${( '00' + calendar.clickDate ).slice( -2 )}`
+          input.inputMoneyValue('',inputDate,'',1,0,inputDate);
         } else {
-          input.inputMoneyValue('',`${toYear}-${( '00' + (toMonth) ).slice( -2 )}-${( '00' + toDate ).slice( -2 )}`,'',1);
+          let inputDate = `${toYear}-${( '00' + (toMonth) ).slice( -2 )}-${( '00' + toDate ).slice( -2 )}`
+          input.inputMoneyValue('',inputDate,'',1,0,inputDate);
         }
       }
     })
@@ -59,9 +62,15 @@ class Input {
 
     // 登録ボタン
     inputBtn.addEventListener('click',function(e) {
-      let inputDay = new Date(input.inputForm.date.value).getDay()
-      let inputDate = input.inputForm.date.value.replace(/-/g, "")
-      input.addInputMoney(Number(input.inputForm.money.value),inputDate,input.inputForm.memo.value,inputDay);
+      let inputDay = new Date(input.inputForm.date.value).getDay();
+      let inputDate = input.inputForm.date.value.replace(/-/g, "");
+      let inputLoop;
+      if (input.LoopMode === false) {
+        inputLoop = input.LoopMode;
+      } else {
+        inputLoop = input.inputForm.loopEnd.value.replace(/-/g, "");
+      }
+      input.addInputMoney(Number(input.inputForm.money.value),inputDate,input.inputForm.memo.value,inputDay,inputLoop);
     })
 
     // 閉じるボタン 削除ボタン
@@ -70,10 +79,8 @@ class Input {
         input.editMode = false;
         input.closeBtn.textContent = '閉じる';
         if (input.moneyMode === 'history') {
-          console.log(setting.hiMoney.history[`${year}${( '00' + (month) ).slice( -2 )}${( '00' + (weeks[calendar.moneyLine][calendar.moneyColumn].date) ).slice( -2 )}`]);
           setting.hiMoney.history[`${year}${( '00' + (month) ).slice( -2 )}${( '00' + (weeks[calendar.moneyLine][calendar.moneyColumn].date) ).slice( -2 )}`].splice(input.editNum,1);
         } else {
-          console.log(setting.hiMoney.receipts);
           setting.hiMoney.receipts.splice([weeks[calendar.moneyLine][calendar.moneyColumn].fixed[input.editNum].index],1);
         }
         moneyInfoBox.classList.remove('active');
@@ -86,15 +93,33 @@ class Input {
     this.inputForm.frequency.addEventListener('change', function(){
       input.changeFrequency();
     });
+
+    this.inputForm.loopLimit.addEventListener('change', function(){
+      input.changeLoop();
+    });
   }
 
   changeFrequency() {
     if (this.inputForm.frequency.selectedIndex === 1) {
       this.loopMoney.style.display = 'none';
       this.moneyMode = 'history';
+      this.inputForm.loopLimit.options[0].selected = true;
+      this.loopMoneyLimit.style.display = 'none';
+      this.inputForm.loopEnd.value = this.inputForm.date.value;
     } else {
       this.loopMoney.style.display = 'block';
       this.moneyMode = 'fixed';
+    }
+  }
+
+  changeLoop() {
+    if (this.inputForm.loopLimit.selectedIndex === 0) {
+      this.loopMoneyLimit.style.display = 'none';
+      this.LoopMode = false;
+      this.inputForm.loopEnd.value = this.inputForm.date.value;
+    } else {
+      this.loopMoneyLimit.style.display = 'block';
+      this.LoopMode = this.inputForm.loopEnd.value;
     }
   }
 
@@ -142,19 +167,32 @@ class Input {
               frequencyValue = 4;
               break;
           }
-        input.inputMoneyValue(editInputMoney,editInputDate,editInputMemo,frequencyValue);
+          let loopValue;
+          let loopDate;
+          if (setting.hiMoney.receipts[weeks[calendar.moneyLine][calendar.moneyColumn].fixed[input.editNum].index].endDate === false) {
+            loopValue = 0;
+            loopDate = editInputDate;
+          } else {
+            loopDate = setting.hiMoney.receipts[weeks[calendar.moneyLine][calendar.moneyColumn].fixed[input.editNum].index].endDate;
+            const loopInputYear = loopDate.substring(0, 4);  //2022
+            const loopInpuMmonth = loopDate.substring(4, 6); //02
+            const loopInputDay = loopDate.substring(6, 8);   //05
+            loopDate = `${loopInputYear}-${loopInpuMmonth}-${loopInputDay}`
+            loopValue = 1;
+          }
+        input.inputMoneyValue(editInputMoney,editInputDate,editInputMemo,frequencyValue,loopValue,loopDate);
       } else {
-        input.inputMoneyValue(editInputMoney,editInputDate,editInputMemo,1);
+        input.inputMoneyValue(editInputMoney,editInputDate,editInputMemo,1,0,editInputDate);
       }
     } else {
       input.editMode = false;
       input.closeBtn.textContent = '閉じる';
-      input.inputMoneyValue('',editInputDate,'',1);
+      input.inputMoneyValue('',editInputDate,'',1,0,editInputDate);
     }
   }
 
   // 入力画面の初期値設定
-  inputMoneyValue(moneyValue,dateValue,memoValue,frequencyValue) {
+  inputMoneyValue(moneyValue,dateValue,memoValue,frequencyValue,loopValue,loopDate) {
     const inputBox = document.querySelector('.input');
     document.querySelector(`.select`).classList.remove('select');
     inputBox.classList.add('select');
@@ -162,67 +200,74 @@ class Input {
     input.inputForm.date.value = dateValue;
     input.inputForm.memo.value = memoValue;
     input.inputForm.frequency.options[frequencyValue].selected = true;
+    input.inputForm.loopLimit.options[loopValue].selected = true;
+    input.inputForm.loopEnd.value = loopDate;
     input.changeFrequency();
+    input.changeLoop()
   }
 
   // 登録処理
-  addInputMoney(inputMoney,inputDate,inputMemo,inputDay) {
+  addInputMoney(inputMoney,inputDate,inputMemo,inputDay,inputLoop) {
     if (inputMoney && inputDate) {
-      moneyInfoBox.classList.remove('active');
-      if (!input.inputMoneyisAdd) {
-        inputMoney = -(inputMoney);
-      }
-      if (input.editMode) {
-        if (input.oldMoneyMode === 'history') {
-          setting.hiMoney.history[`${year}${( '00' + (month) ).slice( -2 )}${( '00' + (weeks[calendar.moneyLine][calendar.moneyColumn].date) ).slice( -2 )}`].splice(input.editNum,1);
+      if (input.inputForm.date.value <= input.inputForm.loopEnd.value || inputLoop === false) {
+        moneyInfoBox.classList.remove('active');
+        if (!input.inputMoneyisAdd) {
+          inputMoney = -(inputMoney);
+        }
+        if (input.editMode) {
+          if (input.oldMoneyMode === 'history') {
+            setting.hiMoney.history[`${year}${( '00' + (month) ).slice( -2 )}${( '00' + (weeks[calendar.moneyLine][calendar.moneyColumn].date) ).slice( -2 )}`].splice(input.editNum,1);
+          } else {
+            setting.hiMoney.receipts.splice([weeks[calendar.moneyLine][calendar.moneyColumn].fixed[input.editNum].index],1);
+          }
+        }
+        if (input.moneyMode === 'history') {
+          if (!setting.hiMoney.history[inputDate]) {
+            setting.hiMoney.history[inputDate] = [];
+          }
+    
+          setting.hiMoney.history[inputDate].push(
+            {
+              memo:inputMemo,
+              sum:inputMoney,
+            }
+          )
         } else {
-          setting.hiMoney.receipts.splice([weeks[calendar.moneyLine][calendar.moneyColumn].fixed[input.editNum].index],1);
-        }
-      }
-      if (input.moneyMode === 'history') {
-        if (!setting.hiMoney.history[inputDate]) {
-          setting.hiMoney.history[inputDate] = [];
-        }
-  
-        setting.hiMoney.history[inputDate].push(
-          {
-            memo:inputMemo,
-            sum:inputMoney,
+          let dateNum;
+          if (!setting.hiMoney.receipts[this.editNum]) {
+            setting.hiMoney.history[inputDate] = [];
           }
-        )
+          switch (input.inputForm.frequency.options[input.inputForm.frequency.selectedIndex].value) {
+            case 'date':
+              dateNum = [0];
+              break;
+            case 'week':
+              dateNum = [inputDay];
+              break;
+            case 'month':
+              dateNum = [inputDate.slice(-2)];
+              break;
+            case 'year':
+              dateNum = [[month,inputDate.slice(-2)]];
+              break;
+          }
+          setting.hiMoney.receipts.push(
+            {
+              frequency:[input.inputForm.frequency.options[input.inputForm.frequency.selectedIndex].value][0],
+              memo:inputMemo,
+              startDate:inputDate,
+              endDate:inputLoop,
+              num:dateNum,
+              sum:inputMoney,
+            }
+          )
+        }
+        
+        changeScreen(screenMode);
+        setting.save();
       } else {
-        let dateNum;
-        if (!setting.hiMoney.receipts[this.editNum]) {
-          setting.hiMoney.history[inputDate] = [];
-        }
-        switch (input.inputForm.frequency.options[input.inputForm.frequency.selectedIndex].value) {
-          case 'date':
-            dateNum = [0];
-            break;
-          case 'week':
-            dateNum = [inputDay];
-            break;
-          case 'month':
-            dateNum = [inputDate.slice(-2)];
-            break;
-          case 'year':
-            dateNum = [[month,inputDate.slice(-2)]];
-            break;
-        }
-
-        setting.hiMoney.receipts.push(
-          {
-            frequency:[input.inputForm.frequency.options[input.inputForm.frequency.selectedIndex].value][0],
-            memo:inputMemo,
-            startDate:inputDate,
-            num:dateNum,
-            sum:inputMoney,
-          }
-        )
+        this.inputError.textContent = '開始日が終了日を超えています';
       }
-      
-      changeScreen(screenMode);
-      setting.save();
     } else {
       this.inputError.textContent = '金額または日付が入力されていません';
     }
